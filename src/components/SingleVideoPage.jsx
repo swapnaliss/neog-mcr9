237888f9a4efc662aa51f490bcec26aaebf35df6
swapnaliss <swapnalisbhargude123@gmail.com>
2023-07-29 19:02:04 +0530

    watchlist functionality

--- src/components/SingleVideoPage.jsx
+++ src/components/SingleVideoPage.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useParams } from "react-router-dom";
 import { videos } from "./VideosData";
 import MoreVideos from "./MoreVideos";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {
-  GiNotebook
-} from "react-icons/gi";
-import {MdWatchLater} from "react-icons/md";
-import {AiFillPlayCircle} from "react-icons/ai";
+import { GiNotebook } from "react-icons/gi";
+import { MdWatchLater } from "react-icons/md";
+import { AiFillPlayCircle } from "react-icons/ai";
 const SingleVideoPage = () => {
+  const [currentVideo, setCurrentVideo] = useState(null);
+  const [watchLaterList, setWatchLaterList] = useState([]);
+  const [isAlreadyInWatchList, setisAlreadyInWatchList] = useState(false);
   const { id } = useParams();
   const Id = parseInt(id, 10);
 
   const FindVideos = videos.find((video) => video._id === Id);
+
+  useEffect(() => {
+    const storedWatchLaterList = localStorage.getItem("watchLaterList");
+    if (storedWatchLaterList) {
+      setWatchLaterList(JSON.parse(storedWatchLaterList));
+    }
+  }, [Id]);
+
+  const handleAddToWatchLater = useCallback(
+    (FindVideos) => {
+      localStorage.setItem(
+        "watchLaterList",
+        JSON.stringify([...watchLaterList, FindVideos])
+      );
+    },
+    [watchLaterList, FindVideos, Id]
+  );
 
   return (
     <div>
@@ -25,21 +43,30 @@ const SingleVideoPage = () => {
               width="560"
               height="315"
               src={FindVideos.src}
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
             <div>
-            <h5>{FindVideos.title}</h5>
-            <button><GiNotebook className="nav-icon" color="blue" /></button>
-            <button><MdWatchLater className="nav-icon" color="blue" /></button>
-            <button>< AiFillPlayCircle className="nav-icon" color="blue"/></button>
+              <h5>{FindVideos.title}</h5>
+              <button>
+                <GiNotebook className="nav-icon" color="blue" />
+              </button>
+              <button
+                onClick={() =>
+                  handleAddToWatchLater(FindVideos, isAlreadyInWatchList)
+                }
+              >
+                <MdWatchLater className="nav-icon" color="blue" />
+              </button>
+              <button>
+                <AiFillPlayCircle className="nav-icon" color="blue" />
+              </button>
             </div>
           </div>
         </Col>
         <Col sm={4}>
           <p>More Videos :</p>
-          <MoreVideos category={FindVideos.category}  />
+          <MoreVideos category={FindVideos.category} />
         </Col>
       </Row>
     </div>
--- src/components/WatchLater.jsx
+++ src/components/WatchLater.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import { Container, Row, Col, Card} from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const WatchLater = () => {
+  const [watchLaterList, setWatchLaterList] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedWatchLaterList = localStorage.getItem("watchLaterList");
+    if (storedWatchLaterList) {
+      setWatchLaterList(JSON.parse(storedWatchLaterList));
+    }
+  }, [setWatchLaterList]);
+
   return (
-    <div>WatchLater</div>
+    <div>
+       <Container>
+            <Row className="justify-content-center">
+            {watchLaterList.map((videos) => (
+              <Col
+                key={videos._id}
+                className="mb-4"
+              >
+                <Card
+                  className="h-100"
+                  onClick={() => navigate(`/SingleVideoPage/${videos._id}`)}
+                >
+                  <Card.Img variant="top" src={videos.thumbnail} />
+                  <p>{videos.title}</p>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+    </div>
   )
 }
 
