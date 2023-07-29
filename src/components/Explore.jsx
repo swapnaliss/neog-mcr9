418f4f255b3dacc51722c756bf328d4025d7b806
swapnaliss <swapnalisbhargude123@gmail.com>
2023-07-29 22:03:52 +0530

    explore page functionality added

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { videos } from "./VideosData";
+import { Container, Row, Col, Card, Form } from 'react-bootstrap';
 
 const Explore = () => {
-  return (
-    <div>Explore</div>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState('');
 
-export default Explore
+  const filteredVideos = videos.filter(
+    (video) =>
+      video.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      video.category.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  return (
+    <Container>
+      <h3>Explore</h3>
+      {/* Search bar */}
+      <Form.Group controlId="searchForm">
+        <Form.Control
+          type="text"
+          placeholder="Search by title or Category.."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          
+        />
+      </Form.Group>
+
+      <Row className="justify-content-center">
+        {filteredVideos.map((video) => (
+          <Col key={video._id} xs={12} sm={6} md={4} lg={3} className="mb-4 mt-5">
+            <Card className="h-100" mt-10>
+              <Card.Img variant="top" src={video.thumbnail} />
+              <p>{video.category}</p>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Explore;
--- src/components/SingleVideoPage.jsx
+++ src/components/SingleVideoPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { videos } from "./VideosData";
 import MoreVideos from "./MoreVideos";
@@ -7,11 +7,20 @@ import Col from "react-bootstrap/Col";
 import { GiNotebook } from "react-icons/gi";
 import { MdWatchLater } from "react-icons/md";
 import { AiFillPlayCircle } from "react-icons/ai";
+import Button from "react-bootstrap/Button";
+import Overlay from "react-bootstrap/Overlay";
+import Popover from "react-bootstrap/Popover";
+
 const SingleVideoPage = () => {
   const [currentVideo, setCurrentVideo] = useState(null);
   const [watchLaterList, setWatchLaterList] = useState([]);
   const [isAlreadyInWatchList, setisAlreadyInWatchList] = useState(false);
   const [playlists, setPlaylists] = useState([]);
+  const [notes, setNotes] = useState("");
+
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
 
   const { id } = useParams();
   const Id = parseInt(id, 10);
@@ -28,6 +37,13 @@ const SingleVideoPage = () => {
     if (storedPlaylists) {
       setPlaylists(JSON.parse(storedPlaylists));
     }
+
+     // Load notes from local storage on component mount
+     const storedNotes = localStorage.getItem(`notes-${Id}`);
+     if (storedNotes) {
+       setNotes(storedNotes);
+     }
+ 
 
     // Find the current video
     const findVideo = videos.find((video) => video._id === Id);
@@ -46,15 +62,24 @@ const SingleVideoPage = () => {
     [watchLaterList, currentVideo, Id]
   );
 
-  const handleAddToPlaylist = useCallback(
-    () => {
-      localStorage.setItem(
-        "playlists",
-        JSON.stringify([...playlists, currentVideo])
-      );
-    },
-    [watchLaterList, currentVideo, Id]
-  );
+  const handleAddToPlaylist = useCallback(() => {
+    localStorage.setItem(
+      "playlists",
+      JSON.stringify([...playlists, currentVideo])
+    );
+  }, [watchLaterList, currentVideo, Id]);
+
+  const handleAddNote = useCallback((event) => {
+    localStorage.setItem(
+      `notes-${Id}`,
+      JSON.stringify(event.target.value)
+    );
+  }, [Id]);
+
+  const handleClick = (event) => {
+    setShow(!show);
+    setTarget(event.target);
+  };
 
   return (
     <div>
@@ -71,9 +96,14 @@ const SingleVideoPage = () => {
             ></iframe>
             <div>
               <h5>{currentVideo?.title}</h5>
-              <button>
-                <GiNotebook className="nav-icon" color="blue" />
+              <button ref={ref}>
+                <GiNotebook
+                  onClick={handleClick}
+                  className="nav-icon"
+                  color="blue"
+                />
               </button>
+
               <button
                 onClick={() =>
                   handleAddToWatchLater(currentVideo, isAlreadyInWatchList)
@@ -81,7 +111,7 @@ const SingleVideoPage = () => {
               >
                 <MdWatchLater className="nav-icon" color="blue" />
               </button>
-              <button  onClick={() => handleAddToPlaylist("Playlist 1")}>
+              <button onClick={() => handleAddToPlaylist("Playlist 1")}>
                 <AiFillPlayCircle className="nav-icon" color="blue" />
               </button>
             </div>
@@ -91,6 +121,25 @@ const SingleVideoPage = () => {
           <p>More Videos :</p>
           <MoreVideos category={currentVideo?.category} />
         </Col>
+        <Overlay
+          show={show}
+          target={target}
+          placement="bottom"
+          container={ref}
+          containerPadding={20}
+        >
+          <Popover id="popover-contained">
+            <Popover.Header as="h3">Add New Notes</Popover.Header>
+            <Popover.Body>
+              <textarea
+                value={notes}
+                onChange={handleAddNote}
+                placeholder="Add notes..."
+              />
+              <Button>Add Notes</Button>
+            </Popover.Body>
+          </Popover>
+        </Overlay>
       </Row>
     </div>
   );
